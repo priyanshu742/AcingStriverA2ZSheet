b15b730277f198325fc8a50b5289fc7922276637
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-06 15:57:44 +0530

    Trying switch statement

---
+++ switch.java
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+/*
+
+Given the integer day denoting the day number, print on the screen which day of the week it is. Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+
+Ensure only the 1st letter of the answer is capitalised.
+
+Example 1
+
+Input: day = 3
+
+Output: Wednesday
+
+Example 2
+
+Input: day = 8
+
+Output: Invalid
+
+Constraints
+
+0 <= day <= 50
+
+
+*/
+
+
+class Solution 
+{
+    public void whichWeekDay(int day) 
+    {
+        switch(day)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
