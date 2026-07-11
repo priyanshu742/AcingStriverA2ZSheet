ffcb1b81094fd20169a13fc02a290c797e4648fb
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-11 17:57:12 +0530

    Pattern8

---
+++ LearnTheBasics/Patterns/Pattern8.java
@@ -0,0 +1,43 @@
+package LearnTheBasics.Patterns;
+
+
+/*
+
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*********
+ *******
+  *****
+   ***
+    *
+
+Print the pattern in the function given to you.
+
+Constraints
+
+1 <= n <= 100
+
+*/
+
+
+class Solution 
+{
+    public void pattern8(int n) 
+    {
+        for(int i=n;i>=1;i--)
+        {
+            for(int s =n-i ; s>=1 ;s--)
+            {
+                System.out.print(" ");
+            }
+            for(int j =(2*i)-1 ; j>=1 ; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
