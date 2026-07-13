c5e4c3de4ca52bfd39fcd13bf6130b2e32f2ae2a
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-13 16:54:25 +0530

    Pattern15

---
+++ LearnTheBasics/Patterns/Pattern15.java
@@ -0,0 +1,39 @@
+package LearnTheBasics.Patterns;
+
+
+
+/*
+ 
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+ABCDE
+ABCD
+ABC
+AB
+A
+
+Print the pattern in the function given to you.
+
+Constraints
+1 <= n <= 26
+
+*/
+
+
+
+class Solution 
+{
+    public void pattern15(int n) 
+    {
+        for(int i= n; i>=1 ; i--)
+        {
+            for(int j=1 ; j<=i ; j++)
+            {
+                System.out.print((char)('A' + (j-1)));
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
