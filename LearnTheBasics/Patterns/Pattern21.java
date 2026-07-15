3523df7ab2259e019be42b605079ab413ce46342
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-15 20:14:47 +0530

    Pattern21

---
+++ LearnTheBasics/Patterns/Pattern21.java
@@ -0,0 +1,44 @@
+package LearnTheBasics.Patterns;
+
+
+/* 
+
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*****
+*   *
+*   *
+*   *
+*****
+
+Print the pattern in the function given to you.
+
+Constraints
+1 <= n <= 100
+
+*/
+
+
+class Solution 
+{
+    public void pattern21(int n) 
+    {
+        for(int i =1 ; i<=n ; i++)
+        {
+            for(int j =1 ; j <= n ; j++)
+            {
+                if(j==1 || j==n || i==1 || i==n )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
