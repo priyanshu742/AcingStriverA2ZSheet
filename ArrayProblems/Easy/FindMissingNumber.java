1f79ecc88a60f8f6f7caf6d720882b51bfd8a014
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-31 21:59:06 +0530

    Finding the missing number in an array

---
+++ ArrayProblems/Easy/FindMissingNumber.java
@@ -0,0 +1,48 @@
+package ArrayProblems.Easy;
+
+
+/*
+
+Given an integer array of size n containing distinct values in the range from 0 to n (inclusive), return the only number missing from the array within this range.
+
+Example 1
+Input: nums = [0, 2, 3, 1, 4]
+Output: 5
+Explanation:
+nums contains 0, 1, 2, 3, 4 thus leaving 5 as the only missing number in the range [0, 5]
+
+Example 2
+Input: nums = [0, 1, 2, 4, 5, 6]
+Output: 3
+Explanation:
+nums contains 0, 1, 2, 4, 5, 6 thus leaving 3 as the only missing number in the range [0, 6]
+
+
+Constraints
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+
+EASY
+*/
+
+
+class Solution 
+{
+    public int missingNumber(int[] nums) 
+    {
+        int size=nums.length;
+
+        int expectedSum=(size*(size+1))/2;
+
+        int actualSum=0;
+        for(int n:nums)
+        {
+            actualSum=actualSum+n;
+        }
+        return expectedSum-actualSum;
+    }
+}
+    
+
