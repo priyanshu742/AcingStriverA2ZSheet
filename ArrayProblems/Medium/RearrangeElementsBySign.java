3e0668b2010414c4921ccc4028594949a55c6385
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-08-06 19:41:36 +0530

    Rearranging array elements by sign

--- ArrayProblems/Medium/RearrangeElementsBySign.java
+++ ArrayProblems/Medium/RearrangeElementsBySign.java
@@ -1,3 +1,64 @@
+package ArrayProblems.Medium;
+
+/* 
+
+Given an integer array nums of even length consisting of an equal number of positive and negative integers.
+Return the answer array in such a way that the given conditions are met:
+Every consecutive pair of integers have opposite signs.
+For all integers with the same sign, the order in which they were present in nums is preserved.
+The rearranged array begins with a positive integer.
+
+Example 1
+Input : nums = [2, 4, 5, -1, -3, -4]
+Output : [2, -1, 4, -3, 5, -4]
+Explanation:
+The positive number 2, 4, 5 maintain their relative positions and -1, -3, -4 maintain their relative positions
+
+Example 2
+Input : nums = [1, -1, -3, -4, 2, 3]
+Output : [1, -1, 2, -3, 3, -4]
+Explanation:
+The positive number 1, 2, 3 maintain their relative positions and -1, -3, -4 maintain their relative positions
+
+Constraints
+2 <= nums.length <= 105
+1 <= | nums[i] | <= 104
+nums.length is an even number.
+Number of positive and negative numbers are equal.
+
+EASY
+*/
+
+class Solution 
+{
+    public int[] rearrangeArray(int[] nums) 
+    {
+        // OPTIMAL 
+        int posIndex=0;
+        int negIndex=1;
+
+        int ans[]=new int[nums.length];
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>=0)
+            {
+                ans[posIndex]=nums[i];
+                posIndex+=2;
+            }
+            else
+            {
+                ans[negIndex]=nums[i];
+                negIndex+=2;
+            }
+        }
+        return ans;
+    }
+}
+
+
+//FOR ODD ARRAY SIZE
+
 class Solution 
 {
     public int[] rearrangeArray(int[] nums) 
@@ -75,17 +136,5 @@ class Solution
             }
         }
         return nums;
-    }
-}
-
-public class demo
-{
-    public static void main(String[] args) 
-    {
-        Solution s1= new Solution();
-        int nums[]={2, 4, 5, -1, -3, -4,8,8,9,6,-7,-5,-7,-8,-2,-3};
-
-        int result[] = s1.rearrangeArray(nums);
-        System.out.println(java.util.Arrays.toString(result));
     }
 }
